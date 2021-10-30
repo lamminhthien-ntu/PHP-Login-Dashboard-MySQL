f4d630bd559fb2711093651be984a5b83cc15966
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-10-30 15:04:30 +0700

    split header to php file

--- admin/DashboardIndex.php
+++ admin/DashboardIndex.php
@@ -8,41 +8,7 @@
 <body style="">
 	<section id="container">
 		<!--header start-->
-		<header class="header fixed-top clearfix">
-			<!--logo start-->
-			<div class="brand">
-				<a href="http://localhost/shopbanhanglaravel/index.html" class="logo">
-					Admin
-					<!-- notification dropdown end -->
-					<!--  notification end -->
-			</a></div><a href="http://localhost/shopbanhanglaravel/index.html" class="logo">
-			</a><div class="top-nav clearfix"><a href="http://localhost/shopbanhanglaravel/index.html" class="logo">
-				<!--search & user info start-->
-				</a><ul class="nav pull-right top-menu"><a href="http://localhost/shopbanhanglaravel/index.html" class="logo">
-					<li>
-						<input type="text" class="form-control search" placeholder=" Search">
-					</li>
-					<!-- user login dropdown start-->
-					</a>
-                    <li class="dropdown"><a data-toggle="dropdown" class="dropdown-toggle" href="http://localhost/shopbanhanglaravel/all-product#">
-							<img alt="" src="DashboardCSS/2.png">
-							<span class="username">
-								Lâm Minh Thiện
-							</span>
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu extended logout">
-							<li><a href="http://localhost/shopbanhanglaravel/all-product#"><i class=" fa fa-suitcase"></i>Thông tin</a></li>
-							<li><a href="http://localhost/shopbanhanglaravel/all-product#"><i class="fa fa-cog"></i> Cài đặt</a></li>
-							<li><a href="http://localhost/shopbanhanglaravel/logout"><i class="fa fa-key"></i> Đăng xuất</a></li>
-						</ul>
-					</li>
-					<!-- user login dropdown end -->
-
-				</ul>
-				<!--search & user info end-->
-			</div>
-		</header>
+       <?php include ('header.php') ?>
 		<!--header end-->
 		<!--sidebar start-->
 	<?php include ('sidebar.php'); ?>
